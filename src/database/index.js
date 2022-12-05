4a0de3c8b8adf8a87ec0dd5ae89f89f7625814bd
marlleyck <marlleyck@hotmail.com>
2022-12-04 21:22:31 -0300

    feat: add env variables

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,15 @@
+require('dotenv').config()
 const mysql = require('mysql')
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const dbName = process.env.DB_NAME
 
 const connection = mysql.createConnection({
 	host: 'localhost',
-	user: 'root',
-	password: 'linduxz.com',
-	database: 'todolist'
+	user: dbUser,
+	password: dbPassword,
+	database: dbName
 })
 
 connection.connect((err) => {
